1a62c1ce753470cc6ad64656be892ebf7b691f68
AndrryArmor <andrry1@gmail.com>
2020-02-11 18:36:25 +0200

    Added data classes and files

---
+++ TRPZ_Labs/TRPZ_Labs/DataLoader.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPZ_Labs
+{
+    public class DataLoader
+    {
+        private const string goodsFileName = "Goods.data";
+        private const string shopsFileName = "ShopGoods.data";
+        private const string jsonFileName = "Orders.json";
+        private const string dataPath = "..\\..\\Data\\";
+
+        public List<Good> LoadGoods()
+        {
+            var goods = new List<Good>();
+
+            // Good describes as string of "ID, Name, Model, Manufacturer, Decription"  
+            using (var goodsData = new StreamReader(new Uri(dataPath + goodsFileName, UriKind.Relative).ToString()))
+            {
+                while (!goodsData.EndOfStream)
+                {
+                    string[] parameters = goodsData.ReadLine().Split(',');
+                    parameters.Select(parameter => parameter.Trim(' ', '"'));
+
+                    int id = int.Parse(parameters[0]);
+                    string name = parameters[1];
+                    string model = parameters[2];
+                    string manufacturer = parameters[3];
+                    string description = parameters[4];
+                    goods.Add(new Good(id, name, model, manufacturer, description));
+                }
+            }
+            return goods;
+        }
+
+        public List<Shop> LoadShops(List<Good> goods)
+        {
+            var shops = new List<Shop>();
+
+            // Shop describes as "Name, goodID1, price1, goodID2, price2, ..."
+            using (var shopGoodsData = new StreamReader(new Uri(dataPath + shopsFileName, UriKind.Relative).ToString()))
+            {
+                while (!shopGoodsData.EndOfStream)
+                {
+                    string[] parameters = shopGoodsData.ReadLine().Split(',');
+                    parameters.Select(parameter => parameter.Trim(' ', '"'));
+
+                    string shopName = parameters[0];
+                    var items = new List<Item>();
+                    for (int i = 1; i < parameters.Length; i += 2)
+                    {
+                        int goodID = int.Parse(parameters[i]);
+                        Good goodFromId = goods.Find(good => good.ID == goodID);
+                        float price = float.Parse(parameters[i + 1].Replace('.', ','));
+
+                        items.Add(new Item(goodFromId, shopName, price));
+                    }
+
+                    shops.Add(new Shop(shopName, items));
+                }
+            }
+            return shops;
+        }
+
+        public List<Order> DeserializeOrders()
+        {
+            string path = dataPath + jsonFileName;
+            if (File.Exists(path) == false || File.ReadAllText(path) == "")
+                return new List<Order>();
+
+            using (var fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                var jsonSerializer = new DataContractJsonSerializer(typeof(List<Order>));
+                return (List<Order>)jsonSerializer.ReadObject(fs);
+            }
+        }
+
+        public void SerializeOrders(List<Order> orders)
+        {
+            string path = dataPath + jsonFileName;
+            if (orders == null || orders.Count == 0)
+                return;
+
+            using (var fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                var jsonSerializer = new DataContractJsonSerializer(typeof(List<Order>));
+                jsonSerializer.WriteObject(fs, orders);
+            }
+        }
+    }
+}
---
+++ TRPZ_Labs/TRPZ_Labs/OrderingGoodsModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPZ_Labs
+{
+    public class OrderingGoodsModel
+    {
+        private readonly List<Good> goods;
+        private readonly List<Shop> shops;
+        private readonly List<Order> orders;
+
+        public OrderingGoodsModel()
+        {
+            var dataLoader = new DataLoader();
+            goods = dataLoader.LoadGoods();
+            shops = dataLoader.LoadShops(GetGoods());
+            orders = dataLoader.DeserializeOrders();
+        }
+
+        public List<Good> GetGoods()
+        {
+            return goods;
+        }
+
+        public List<Shop> GetShops()
+        {
+            return shops;
+        }
+
+        public List<Order> GetOrders()
+        {
+            return orders;
+        }
+    }
+}
