2b02bd5c622d74877821cddd8db9803aa010e4be
AndrryArmor <andrry1@gmail.com>
2020-04-15 12:04:24 +0300

    Implemented UnityOfWork and Repository patterns

---
+++ TRPZ_Labs/TRPZ_Labs/DataAccessLayer/OrderRepository.cs
@@ -0,0 +1,44 @@
+﻿using OrderingGoods.DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingGoods.DataAccessLayer
+{
+    public class OrderRepository : IRepository<Order>
+    {
+        private OrderingGoodsContext appContext;
+
+        public OrderRepository(OrderingGoodsContext appContext)
+        {
+            this.appContext = appContext;
+        }
+
+        public void Create(Order item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Order item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TRPZ_Labs/TRPZ_Labs/DataAccessLayer/OrderingGoodsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderingGoods.DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingGoods.DataAccessLayer
+{
+    public class OrderingGoodsContext : DbContext
+    {
+        public DbSet<Good> Goods { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public OrderingGoodsContext() { }
+    }
+}
