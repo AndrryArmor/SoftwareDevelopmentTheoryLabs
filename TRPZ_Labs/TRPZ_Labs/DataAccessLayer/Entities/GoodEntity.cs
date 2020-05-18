e6ac4943ec5ed80195b46ab29c25860b9abc60d6
AndrryArmor <andrry1@gmail.com>
2020-05-19 02:16:25 +0300

    Changed entities for migrations (must be before them), updated packages, minor changes

--- TRPZ_Labs/TRPZ_Labs/DataAccessLayer/Entities/GoodEntity.cs
+++ TRPZ_Labs/TRPZ_Labs/DataAccessLayer/Entities/GoodEntity.cs
@@ -4,10 +4,12 @@ namespace OrderingGoods.DataAccessLayer.Entities
 {
     public class GoodEntity
     {
-        [Key]
-        public int ID { get; set; }
+        public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public string Model { get; set; }
+        [Required]
         public string Manufacturer { get; set; }
         public string Description { get; set; }
     }
