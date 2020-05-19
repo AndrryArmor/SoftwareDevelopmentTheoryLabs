8e80311c7a45950d84aafa92c8b74550a87b4e41
AndrryArmor <andrry1@gmail.com>
2020-05-19 20:49:59 +0300

    Implemented canonical Repository pattern

---
+++ TRPZ_Labs/TRPZ_Labs/DataAccessLayer/Repositories/Interfaces/IGoodRepository.cs
@@ -0,0 +1,9 @@
+﻿using OrderingGoods.DataAccessLayer.Entities;
+using System.Collections.Generic;
+
+namespace OrderingGoods.DataAccessLayer.Repository
+{
+    public interface IGoodRepository : IRepository<GoodEntity>
+    {
+    }
+}
